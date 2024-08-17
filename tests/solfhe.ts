fe9ccaff2e78d575fb58ae02fc3ee5c2bc8b49e2
Virjilakrum <asesenep15@gmail.com>
2024-08-18 02:54:48 +0300

    test file update

--- tests/solfhe.ts
+++ tests/solfhe.ts
@@ -1,16 +1,36 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Solfhe } from "../target/types/solfhe";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+// import { Solfhe } from "../target/types/solfhe";
+import { expect } from "chai";
 
 describe("solfhe", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Solfhe as Program<Solfhe>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Initializes the program", async () => {
+    const [statePDA] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("state")],
+      program.programId
+    );
+
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        state: statePDA,
+        authority: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
     console.log("Your transaction signature", tx);
+
+    const stateAccount = await program.account.stateAccount.fetch(statePDA);
+    expect(stateAccount.authority).to.eql(provider.wallet.publicKey);
+    expect(stateAccount.advertiserCount).to.eql(0);
+    expect(stateAccount.userCount).to.eql(0);
+    expect(stateAccount.adCount).to.eql(0);
+    expect(stateAccount.proofCount).to.eql(0);
   });
 });
